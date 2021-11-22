83d98d2904e3dec0ccbc17ce9917b00c9bdb4642
Cory Thompson <cory.thompson98@gmail.com>
2021-11-22 10:42:49 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,12 @@
 var buttonEl = document.querySelector("#save-task");
 console.log(buttonEl)
+
+var buttonEl = docuemnt.querySelector("#save-task");
+var tasksToDoEl = docuemnt.querySelector("#tasks-to-do");
+
+buttonEl.addEventListener("click", function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+});
